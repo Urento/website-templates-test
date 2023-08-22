5c192bebeb97170c753c7fecee12621bba43a4c2
Urento <58522564+Urento@users.noreply.github.com>
2023-08-22 19:59:38 +0000

    sdfjkgdsfbnjkgdfg

---
+++ website-test-1/assets/js/mobile-nav.js
@@ -0,0 +1,9 @@
+const nav = document.querySelector("topnav");
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 768) {
+    nav.style.visibility = "visible";
+  } else {
+    nav.style.visibility = "hidden";
+  }
+});
