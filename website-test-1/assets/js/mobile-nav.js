bfac02d99c9567bd7ade5ecfd4553253e7da0b20
Urento <58522564+Urento@users.noreply.github.com>
2023-08-22 20:27:32 +0000

    sdklfgsdkjhgfbndfg

--- website-test-1/assets/js/mobile-nav.js
+++ website-test-1/assets/js/mobile-nav.js
@@ -1,9 +1,9 @@
 const nav = document.querySelector("topnav");
 
-window.addEventListener("resize", () => {
+/*window.addEventListener("resize", () => {
   if (window.innerWidth <= 768) {
     nav.style.visibility = "visible";
   } else {
     nav.style.visibility = "hidden";
   }
-});
+});*/
