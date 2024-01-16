23b92ede98a262bd9bd4dc42cea98773b36dd0ce
Urento <58522564+Urento@users.noreply.github.com>
2024-01-16 19:00:49 +0000

    added "news ticker" one maybe more miau

--- soft.-dev.-comp.-website/web/src/app/components/navbar/Navbar.tsx
+++ soft.-dev.-comp.-website/web/src/app/components/navbar/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import logo_transparent from "../../logo_transparent.png";
+import logo_transparent from "../../assets/logo_transparent.png";
 
 export type Pages = "Startseite" | "Services" | "Portfolio" | "Kontakt";
 
@@ -47,47 +47,57 @@ export default function Navbar() {
   const navBarLinkTextColor = "text-gray-800 text-xl relative group";
   const boxAroundLink = (type: Pages) =>
     !isLinkActive(convertPageTypeToWindowLocation(type))
-      ? `absolute inset-0 bg-blue-200 text-black opacity-0 group-hover:opacity-20 transition-opacity transform scale-150 rounded-md`
-      : `absolute inset-0 bg-blue-200 opacity-30 transition-opacity transform scale-150 rounded-md`;
+      ? `transition-all duration-300 ease-in-out absolute inset-0 bg-purple-200 text-black opacity-0 group-hover:opacity-20 transition-opacity transform scale-150 rounded-md`
+      : `transition-all duration-300 ease-in-out absolute inset-0 bg-purple-200 opacity-50 transition-opacity transform scale-150 rounded-md`;
   console.log(boxAroundLink);
 
   return (
-    <nav className="bg-white p-4">
-      <div className="container mx-auto">
-        <div className="flex items-center justify-between">
-          <div className="text-black font-bold text-2xl">
-            <a>
-              <Image
+    <>
+      <div className="top-0 left-0 right-0 bg-blue-700 p-1 text-white z-10 flex items-center justify-center">
+        <p className="text-sm">
+          <span className="font-bold">Hinweis:</span> miau
+        </p>
+      </div>
+      <nav className="bg-white p-4">
+        <div className="container mx-auto">
+          <div className="flex items-center justify-between">
+            <div>
+              <a href="/">
+                {/*<Image
                 src={logo_transparent}
                 alt="Logo"
-                width={300}
+                width={350}
                 height={80}
-              />
-            </a>
-          </div>
-          <div className="space-x-12">
-            <a href="#" className={navBarLinkTextColor}>
-              <span className="text-black">Startseite</span>
-              <div className={boxAroundLink("Startseite")}></div>
-            </a>
-            <a href="#" className={navBarLinkTextColor}>
-              <span className="text-black">Services</span>
-              <div className={boxAroundLink("Services")}></div>
-            </a>
-            <a href="#" className={navBarLinkTextColor}>
-              <span className="text-black">Portfolio</span>
-              <div className={boxAroundLink("Portfolio")}></div>
-            </a>
-            <a href="#" className={navBarLinkTextColor}>
-              <span className="text-black">Kontakt</span>
-              <div className={boxAroundLink("Kontakt")}></div>
-            </a>
-            <button className="transition-all duration-300 ease-in-out bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md">
-              Login
-            </button>
+  />*/}
+                <h1 className="text-2xl text-black font-bold">
+                  <span className="text-purple-600">0109</span> Solutions
+                </h1>
+              </a>
+            </div>
+            <div className="space-x-12">
+              <a href="#" className={navBarLinkTextColor}>
+                <span className="text-black">Startseite</span>
+                <div className={boxAroundLink("Startseite")}></div>
+              </a>
+              <a href="#" className={navBarLinkTextColor}>
+                <span className="text-black">Services</span>
+                <div className={boxAroundLink("Services")}></div>
+              </a>
+              <a href="#" className={navBarLinkTextColor}>
+                <span className="text-black">Portfolio</span>
+                <div className={boxAroundLink("Portfolio")}></div>
+              </a>
+              <a href="#" className={navBarLinkTextColor}>
+                <span className="text-black">Kontakt</span>
+                <div className={boxAroundLink("Kontakt")}></div>
+              </a>
+              <button className="transition-all duration-300 ease-in-out bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-md">
+                Login
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
